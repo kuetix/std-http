960edae7cdf410b2414403a91543c1ec4af694a4
Anar Alishov <alishov@gmail.com>
2026-03-10 02:30:18 +0100

    Add package.go file for import and update dependencies Add std-core as module

--- modules/modules.go
+++ modules/modules.go
@@ -5,10 +5,13 @@ import (
 	_ "github.com/kuetix/std-http/modules/api/http/transitions"
 
 	di "github.com/kuetix/container"
+	StdCoreModule "github.com/kuetix/std-core/modules"
 )
 
 func init() {
 	di.Boot()
 }
 
-func Enable() {}
+func Enable() {
+	StdCoreModule.Enable()
+}
