cb9a23bbc3d314c2bb232ebca7d054ae8ed930dc
Anar Alishov <alishov@gmail.com>
2026-04-06 13:34:12 +0200

    refactor: remove unused auth transition import from modules

--- modules/modules.go
+++ modules/modules.go
@@ -1,7 +1,6 @@
 package modules
 
 import (
-	_ "github.com/kuetix/std-http/modules/api/auth/transitions"
 	_ "github.com/kuetix/std-http/modules/api/http/transitions"
 
 	di "github.com/kuetix/container"
